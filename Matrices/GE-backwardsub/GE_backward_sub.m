17d0691838119968a20cd4e8e4ed58ee50c538af
yotodragon <vongphay@gmail.com>
2016-10-01 23:34:07 -0700

    Added GE_backward_sub and GE_backward_elim

---
+++ Matrices/GE-backwardsub/GE_backward_sub.m
@@ -0,0 +1,39 @@
+function x = GE_backward_sub(A,b)
+    [m,n] = size(A);
+    d = size(b,1); 
+%--------------------------------------------------    
+    if n ~= m
+        error('Not a square matrix')
+    end
+    if n ~= d
+        error('Dimension mismatch on the vector and matrix')
+    end
+%--------------------------------------------------
+    % Augmented Matrix
+    M = [A,b];
+    
+    for i = 1:n-1
+        % find the first nonzero element
+        indz = find(M(i:end,i),1)-1;
+        indj = indz+i;
+
+        % if the first nonzero element is not the diagonal element
+        % rowswap with the next positive
+        if indj ~= i
+            M = rowswap(M,i,indj);
+        end
+        
+        % Gaussian Eliminate
+        for j = i+1:n
+            M(j,:) = M(j,:) - M(j,i)/M(i,i)*M(i,:);
+        end
+        
+        % No unique solutions
+        if M(n,n)==0 || isnan(M(n,n))
+            error('no unique solution');
+        end
+    end
+    
+    x = backward_sub(M(1:n,1:n),M(1:n,n+1));
+    
+end
