9d7e1430574d42e7de43b81baa496eb865c8514a
Phay Vong <vongphay@gmail.com>
2016-10-04 09:17:27 -0700

    Update rowswap.m

--- Matrices/GE-backwardsub/rowswap.m
+++ Matrices/GE-backwardsub/rowswap.m
@@ -1,11 +1,10 @@
 % There is a linalg package that swaps rows
 function A = rowswap(A,i,j)
-    n = size(A);
 %--------------------------------------------------
-    if i > n(1)
+    if i > size(A,1)
         error('i is larger than the matrix size!');
     end
-    if j > n(1)
+    if j > size(A,1)
         error('j is larger than the matrix size!');
     end
 %--------------------------------------------------
