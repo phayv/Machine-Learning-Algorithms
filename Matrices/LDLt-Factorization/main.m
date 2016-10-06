5c1a4dd40e9154f04db78ca9c5a1efd7477acf38
Phay Vong <vongphay@gmail.com>
2016-10-06 00:07:10 -0700

    updated main

--- Matrices/LDLt-Factorization/main.m
+++ Matrices/LDLt-Factorization/main.m
@@ -1,4 +1,12 @@
 clear all 
+%Test Example
+%A2 = [4,-1,1;
+%      -1,4.25,2.75;
+%      1,2.75,3.5];
+%b = rand(3,1)  
+%[L,Lt] = LDLt_Factorization(A2);
+%y = forward_substitution_lower(L,b);
+%x = forward_substitution_upper(D*Lt,y)
 
 %works only for positive definite matrices (which are symmetric)
 A = generate_SPD_matrix(5);
